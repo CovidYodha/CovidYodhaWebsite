9ddf5a0363eba756f6ed509c78fd1b5a56456e7f
Aritra Dutta <71974978+aritra1804@users.noreply.github.com>
2021-05-10 14:56:00 +0530

    build_raw_data.py first changes made

---
+++ API/source/build_raw_data.py
@@ -0,0 +1,42 @@
+import pandas as pd # pylint: disable=import-error
+import re
+from pathlib import Path
+import logging
+import sys
+import os
+from urllib.error import HTTPError
+# Set logging level
+logging.basicConfig(stream=sys.stdout,
+                    format="%(message)s",
+                    level=logging.INFO)
+
+def fetch_raw_data_from_api():
+    '''
+    Read all raw data and death and recovery files
+    Pass the latest version of raw data
+    '''
+    i = 1
+    raw_d = []
+    while True:
+        try:
+            url = f"https://api.covid19india.org/csv/latest/raw_data{i}.csv"
+            df = pd.read_csv(url)
+            df.to_csv(f'./tmp/csv/latest/raw_data{i}.csv',index=False)
+            raw_d.append(df)
+            logging.info(f"Fetched raw_data{i} ")
+            i = i+1
+        except HTTPError:
+            current_ver = i-1
+            break
+
+    death_rec = []
+    logging.info(f"Fetching deaths_and_recoveries")
+    url = f"https://api.covid19india.org/csv/latest/death_and_recovered"
+    df = pd.read_csv(f"{url}1.csv")
+    death_rec.append(df)
+    df.to_csv('./tmp/csv/latest/death_and_recovered1.csv',index=False)
+    df = pd.read_csv(f"{url}2.csv")
+    death_rec.append(df)
+    df.to_csv('./tmp/csv/latest/death_and_recovered2.csv',index=False)
+    
+    return raw_d,death_rec,current_ver
