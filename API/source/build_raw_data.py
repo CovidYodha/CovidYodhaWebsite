573736712d09f8c02c211a4610e318ed898b2cd1
Aritra Dutta <71974978+aritra1804@users.noreply.github.com>
2021-05-11 10:51:22 +0530

    Update build_raw_data.py

--- API/source/build_raw_data.py
+++ API/source/build_raw_data.py
@@ -28,8 +28,7 @@ def fetch_raw_data_from_api():
         except HTTPError:
             current_ver = i-1
             break
-
-    death_rec = []
+ death_rec = []
     logging.info(f"Fetching deaths_and_recoveries")
     url = f"https://api.covid19india.org/csv/latest/death_and_recovered"
     df = pd.read_csv(f"{url}1.csv")
@@ -39,4 +38,38 @@ def fetch_raw_data_from_api():
     death_rec.append(df)
     df.to_csv('./tmp/csv/latest/death_and_recovered2.csv',index=False)
     
+    return raw_d,death_rec,current_ver
+
+def fetch_raw_data():
+    '''
+    Read all raw data and death and recovery files
+    Return the latest number of raw data files
+    '''
+    raw_d = []
+    death_rec = []
+    fpath = Path('tmp/csv/latest')
+    
+    i = 1
+    while True:
+        try:
+            df = pd.read_csv(fpath / f"raw_data{i}.csv")
+            raw_d.append(df)
+            logging.info(f"Fetched raw_data{i} ")
+            i = i+1
+        except FileNotFoundError:
+            current_ver = i-1
+            break
+    
+    i = 1
+    while True:
+        try:
+            df = pd.read_csv(fpath / f"death_and_recovered{i}.csv")
+            death_rec.append(df)
+            logging.info(f"Fetched death_and_recovered{i} ")
+            i = i+1
+        except FileNotFoundError:
+            break
+
+    logging.info(f"Data read complete")
+
     return raw_d,death_rec,current_ver
