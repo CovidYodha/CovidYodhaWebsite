50806ebdd36315f903f4454e0ebf8b07a4e42733
Aritra Dutta <71974978+aritra1804@users.noreply.github.com>
2021-05-13 18:53:04 +0530

    Update build_raw_data.py

--- API/source/build_raw_data.py
+++ API/source/build_raw_data.py
@@ -73,3 +73,66 @@ def fetch_raw_data():
     logging.info(f"Data read complete")
 
     return raw_d,death_rec,current_ver
+
+  def fix_rawdata1and2(raw,rec,col_list,sheet_version):
+    '''
+    Raw Data 1 and 2 had different format
+    Select necessary columns and change data types
+    Add death and recovery data to raw_data
+    '''
+    print(f"V{sheet_version} Shape \t: {raw.shape}")
+    
+    # Only choose the valid current statuses
+    raw = raw[raw['Current Status'].isin( ['Hospitalized','Recovered','Deceased','Migrated','Migrated_Other'])].copy()
+    
+    #Prepare neceassary columns
+    raw['Num Cases'] = 1
+    raw['Entry_ID'] = 0
+    raw['Current Status'] = "Hospitalized"
+    raw = raw.fillna('')
+    raw = raw[col_list]
+    
+    # If Detected State is not available, entry is invalid
+    raw = raw[raw['Detected State'] != ''].copy()
+    
+    # Convert Date Announced string to datetime
+    raw['Date Announced'] = pd.to_datetime(raw['Date Announced'],format='%d/%m/%Y')
+    
+
+    # Add Sheet Version Column
+    raw['Sheet_Version'] = sheet_version
+
+    # Only choose the valid current statuses
+    rec = rec[rec['Patient_Status'].isin(['Hospitalized','Recovered','Deceased','Migrated','Migrated_Other'])].copy()
+    
+    # Prepare necessary columns
+    rec['Num Cases'] = 1
+    rec['Entry_ID'] = 0
+    rec['Current Status'] = rec['Patient_Status']
+    rec['Date Announced'] = rec['Date']
+    rec['State code'] = rec['Statecode']
+    rec['Detected City'] = rec['City']
+    rec['Status Change Date'] = ''
+    rec['Contracted from which Patient (Suspected)'] = ''
+    rec['Detected State'] = rec['State']
+    rec['Detected District'] = rec['District']
+    rec['Patient Number'] = rec['Patient_Number (Could be mapped later)']
+    rec['State Patient Number'] = ''
+    rec['Type of transmission'] = ''
+    
+    rec = rec.fillna('')
+    rec = rec[col_list]
+    
+    # If Detected State is not available, entry is invalid
+    rec = rec[rec['Detected State'] != ''].copy()
+    
+    # Convert Date column from string to date
+    rec['Date Announced'] = pd.to_datetime(rec['Date Announced'],format='%d/%m/%Y')
+    
+    # Add sheet version
+    rec['Sheet_Version'] = sheet_version
+    
+    # Add deaths and recoveries to raw data
+    raw = pd.concat([raw,rec],sort=True)
+    
+    return raw
