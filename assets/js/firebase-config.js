8f5164c5945a0f777133d2c010e8428bfa3c85b7
abhinavmaharana <abhinavmaharana.004@gmail.com>
2021-05-16 13:13:28 +0530

    added firebase support

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCjoFObfrlrNU96eiKUUIyGKYuC_nWzyC4",
+    authDomain: "covidyodhawebsite.firebaseapp.com",
+    projectId: "covidyodhawebsite",
+    storageBucket: "covidyodhawebsite.appspot.com",
+    messagingSenderId: "434959698791",
+    appId: "1:434959698791:web:e0f9dd590c456744097dc2",
+    measurementId: "G-JPN2FFJTK7"
+  };
